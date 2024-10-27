68cd592fc18f85158f52c82106abac0477d6bd4c
Yzyjaj <2302165950@qq.com>
2024-10-27 17:29:36 +0800

    third

---
+++ src/main/java/com/hnu/service/DataManageService.java
@@ -0,0 +1,10 @@
+package com.hnu.service;
+
+import com.hnu.pojo.DataManage;
+
+import java.util.List;
+
+public interface DataManageService {
+
+    List<DataManage> list();
+}
---
+++ src/test/java/com/hnu/rlplatform/RlPlatformDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnu.rlplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlPlatformDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
